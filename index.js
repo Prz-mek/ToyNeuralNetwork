d188270550fc68a6c97168b152a57867dc0c9e13
Prz-mek <kwasniewski.pj@gmail.com>
2021-12-31 23:26:22 +0100

    Demo

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// visualization
 const paintPerceptron = (ctx, x, y, r, activation, m = 1) => {
     if (m !== 0) {
         ctx.beginPath();
@@ -309,3 +310,76 @@ class Chart {
         }
     }
 }
+
+
+// main
+const canvNet = document.getElementById('graph');
+const ctxNet = canvNet.getContext('2d');
+
+const canvDraw = document.getElementById('drawing');
+const ctxDraw = canvDraw.getContext('2d');
+
+const canvChart = document.getElementById('chart');
+const ctxChart = canvChart.getContext('2d');
+
+const getWidthNeural = () => document.getElementById('neural').offsetWidth;
+const getWidthOutput = () => 0.8 * document.getElementById('output').offsetWidth;
+const getHeight = () => document.getElementById('neural').offsetHeight;
+
+const sheet = new Sheet(ctxDraw, () => 224, () => 224, 8);
+let net;
+const chart = new Chart(10, () => getWidthOutput(), () => 300);
+
+// setting canvas size
+const setCanvNetDimensions = () => {
+    canvNet.width = getWidthNeural();
+    canvNet.height = getHeight();
+    canvChart.width = getWidthOutput();
+    canvChart.height = 300;
+};
+
+// painting on load and resize
+const paint = () => {
+    net.countPositions();
+    net.paint(ctxNet);
+    chart.paint(ctxChart);
+};
+
+// geting JSON
+const getModel = async (model) => {
+    try {
+        const res = await fetch(`${window.location}/${model}`);
+        const data = await res.json();
+        return data;
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+const changeModel = (model) => {
+    net = new NetworkView(model, getWidthNeural, getHeight, 0.05);
+}
+
+const init = () => {
+    changeModel(sigmoidNet);
+    canvDraw.width = 224;
+    canvDraw.height = 224;
+    setCanvNetDimensions();
+    paint();
+};
+
+init();
+window.addEventListener('resize', () => {
+    setCanvNetDimensions();
+    paint();
+});
+
+document.getElementById('netModel1').onclick = () => changeModel(sigmoidNet);
+document.getElementById('netModel2').onclick = () => changeModel(tanhNet);
+
+canvDraw.addEventListener('mousedown', e => sheet.mouseDownPaint(e));
+canvDraw.addEventListener('mouseup', () => sheet.mouseUpPaint());
+canvDraw.addEventListener('mousemove', e => sheet.draw(e));
+
+document.getElementById('check').onclick = () => sheet.predict(net, ctxNet, ctxChart);
+document.getElementById('erase').onclick = () => sheet.clear();
